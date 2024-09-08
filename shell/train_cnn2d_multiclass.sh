e5e136123d2858bad5835573fef46366ae96b3cd
Paulo Lacerda <pclacerda@gmail.com>
2024-09-08 12:00:21 -0300

    update variables

--- shell/train_cnn2d_multiclass.sh
+++ shell/train_cnn2d_multiclass.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 conda init
 conda activate pulmo-sense
-python -m scripts.train.train_cnn2d_multiclass --dataset ccccii --num_epochs 20 --k 5 --i 1 --batch_size 16 --learning_rate 0.0005 --max_samples 100
+timestamp=$(date +"%Y%m%d_%H%M%S")
+python -m scripts.train.train_cnn2d_multiclass --dataset ccccii --num_epochs 20 --k 5 --i 1 --batch_size 16 --learning_rate 0.0005 > logs/train_cnn2d_multiclass_$timestamp.log 2>&1 &
