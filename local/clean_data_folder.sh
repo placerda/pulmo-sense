12b9c893f2f468fd48bbbd6775ce897a0e26fc68
Paulo Lacerda <pclacerda@gmail.com>
2024-12-26 10:10:23 -0300

    refactoring

--- local/clean_data_folder.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Define the base directory
-BASE_DIR="data"
-
-# Find and delete files starting with .azDownload
-find "$BASE_DIR" -type f -name ".azDownload*" -exec rm -f {} +
-
-echo "Deletion complete."
